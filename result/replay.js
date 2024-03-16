ae660bd69bd08393c94c10a02a06190ba5f50f7e
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-03-17 01:14:25 +0530

    added replay functionality

---
+++ result/replay.js
@@ -0,0 +1,8 @@
+export function addPlayAgainListener() {
+    const playAgainButton = document.querySelector('.play-again-button');
+    playAgainButton.addEventListener('click', playAgain);
+}
+
+function playAgain() {
+    window.location.href = '../index.html';
+}
