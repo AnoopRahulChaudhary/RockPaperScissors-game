439d1599b1ac75c78b56b3fc5dc47ef52187050c
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-03-17 16:14:16 +0530

    Added function to update images border and background in result page

--- result/updatePickedImages.js
+++ result/updatePickedImages.js
@@ -10,12 +10,14 @@ function updateUsersPickedImage() {
     const imageValue =  getQueryValueFromUrl(keyForUserImageSelection);
     const userImageHolder = document.querySelector('.human-picked-status .picked-img-container img');
     userImageHolder.setAttribute('src', getImagePath(imageValue));
+    updateCssForUserPickedImage(imageValue);
 }
 
 function updateComputersPickedImage() {
     const imageValue =  getQueryValueFromUrl(keyForComputerImageSelection);
     const computerImageHolder = document.querySelector('.computer-picked-status .picked-img-container img');
     computerImageHolder.setAttribute('src', getImagePath(imageValue));
+    updateCssForComputerPickedImage(imageValue);
 }
 
 function  getQueryValueFromUrl(keyForImageSelection) {
@@ -29,4 +31,29 @@ function getPageUrl() {
 
 function getImagePath(imageValue) {
     return '../assets/images/'+imageValue+'.png';
+}
+
+function updateCssForUserPickedImage(imageValue) {
+    const userImageContainer = document.querySelector('.human-picked-status .picked-img-container');
+    userImageContainer.setAttribute('class', getAllClassesForImageContainer(imageValue));
+    
+    const userImageTag = document.querySelector('.human-picked-status .picked-img-container img');
+    userImageTag.setAttribute('alt', getAltValueForPickedImage(imageValue));
+}
+
+function updateCssForComputerPickedImage(imageValue) {
+    const computerImageContainer = document.querySelector('.computer-picked-status .picked-img-container');
+    computerImageContainer.setAttribute('class', getAllClassesForImageContainer(imageValue));
+
+    const computerImageTag = document.querySelector('.computer-picked-status .picked-img-container img');
+    computerImageTag.setAttribute('alt', getAltValueForPickedImage(imageValue));
+}
+
+function getAllClassesForImageContainer(imageValue) {
+    return 'picked-img-container' + ' ' +
+         imageValue + '-border-color';
+}
+
+function getAltValueForPickedImage(imageValue) {
+    return imageValue + ' image';
 }
